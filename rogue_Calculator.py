7201093616f320647bf88710dee6e80bc765f3f6
Static-Line <47070839+Static-Line@users.noreply.github.com>
2020-06-29 11:07:26 -0500

    Add files via upload

---
+++ rogue_Calculator.py
@@ -0,0 +1,8 @@
+number_1 = int(input('Enter your first number: '))//Crit
+number_2 = int(input('Enter your second number: '))//agility
+number_3 = int(input('Enter your third number: '))//skillmodifier
+number_4 = int(input('Enter your fourth number: '))//bounuses
+number_5 = int(input('Enter your fifth number: '))//agilityfactor
+
+print(* [number_1] + number_2 * number_5 + [number_3] + [number_4]))
+print(* [Crit] + Agility* AF + [Skill Modifier] + [Bonuses])
